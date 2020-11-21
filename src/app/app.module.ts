d354c815580bae9a4e95477d36de200414938c78
Apurba <apurba@excellencetech.com>
2020-11-21 12:44:48 +0530

    Upd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import {MinieditorVcModule} from 'minieditor-vc';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MinieditorVcModule
   ],
   providers: [],
   bootstrap: [AppComponent]
