4fb67aa8cd20e840af1b73d320b6099304e191f9
Alec Aldrine Lakra <lakraalec123@gmail.com>
2021-02-15 12:52:41 +0530

    editor menu

--- src/app/melo-mini-editor/melo-mini-editor.module.ts
+++ src/app/melo-mini-editor/melo-mini-editor.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EditorContainerComponent } from './editor-container/editor-container.component';
 import { ToolsModule } from './tools/tools.module';
+import { EditorMenuComponent } from './editor-menu/editor-menu.component';
 
 
 
 @NgModule({
-  declarations: [EditorContainerComponent],
+  declarations: [EditorContainerComponent, EditorMenuComponent],
   imports: [
     CommonModule,
     ToolsModule
