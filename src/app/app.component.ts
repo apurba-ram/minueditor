b997b7c7468594e3506a7ad5315d553ffa26f837
Alec Aldrine Lakra <lakraalec123@gmail.com>
2021-02-15 14:19:15 +0530

    editor config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ export class AppComponent {
   modelvalue = '';
   editorConfig = {
     file: true,
-    mentionedNames: [{ _id: 'asd', name: 'Alec'}, { _id: 'asd', name: 'Pappu'}, { _id: 'asd', name: 'Joyce'}],
+    mentionedNames: [{ _id: 'asd', name: 'Alec'}, { _id: 'asg5d', name: 'Pappu'}, { _id: 'asdqq', name: 'Joyce'}],
     mentionedDates: ['19-02-2020', '11-02-2020', '12-02-2020', '14-02-2020'],
     buttonName: 'Comment',
     fontColor: true,
--- src/app/melo-mini-editor/editor-menu/editor-menu.component.ts
+++ src/app/melo-mini-editor/editor-menu/editor-menu.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import EditorConfig from '../config-interface';
 @Component({
   selector: 'app-editor-menu',
   templateUrl: './editor-menu.component.html',
@@ -7,7 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditorMenuComponent implements OnInit {
 
-  constructor() { }
+  @Input() editorConfig: EditorConfig;
+  constructor() {
+    this.editorConfig = {
+      file: false,
+      mentionedNames: [],
+      mentionedDates: [],
+      colorPalette: ['#FF5630', '#000000', '#414141', '#36B37E',
+                     '#6554C0', '#FF7A00', '#008299', ' #1E5DD3',
+                     '#F0B819', '#00FFF7'],
+      buttonName: '',
+      fontColor: false,
+      highlightColor: false
+    };
+  }
 
   ngOnInit(): void {
   }
