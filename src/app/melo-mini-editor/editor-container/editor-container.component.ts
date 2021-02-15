b535a7251e78ee621ebb4432449808c5c79a29f2
Alec Aldrine Lakra <lakraalec123@gmail.com>
2021-02-15 13:59:31 +0530

    editor container

--- src/app/melo-mini-editor/editor-container/editor-container.component.ts
+++ src/app/melo-mini-editor/editor-container/editor-container.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
+import EditorConfig from '../config-interface';
 @Component({
   selector: 'app-editor-container',
   templateUrl: './editor-container.component.html',
   styleUrls: ['./editor-container.component.less']
 })
-export class EditorContainerComponent implements OnInit {
-
-  constructor() { }
+export class EditorContainerComponent implements OnInit, OnChanges {
+  @Input() editorConfig: EditorConfig;
+  constructor() {
+    this.editorConfig = {
+      file: false,
+      mentionedNames: [],
+      mentionedDates: [],
+      colorPalette: ['#FF5630', '#000000', '#414141', '#36B37E',
+                     '#6554C0', '#FF7A00', '#008299', ' #1E5DD3',
+                     '#F0B819', '#00FFF7'],
+      buttonName: '',
+      fontColor: false,
+      highlightColor: false
+    };
+  }
 
   ngOnInit(): void {
+  }
+
+  ngOnChanges(): void {
   }
 
 }
