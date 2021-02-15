a697547cef620a655f1e036507ae217b7900a2ff
Alec Aldrine Lakra <lakraalec123@gmail.com>
2021-02-15 12:20:57 +0530

    minueditor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {FormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {MinieditorVcModule} from 'minieditor-vc';
+import { MinieditorVcModule } from 'minieditor-vc';
 @NgModule({
   declarations: [
     AppComponent
