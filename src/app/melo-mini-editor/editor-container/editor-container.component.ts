73141483495c26019f2e12f725646590d7f0e87f
Alec Aldrine Lakra <lakraalec123@gmail.com>
2021-02-15 14:58:10 +0530

    editor value

--- src/app/melo-mini-editor/editor-container/editor-container.component.ts
+++ src/app/melo-mini-editor/editor-container/editor-container.component.ts
@@ -1,12 +1,22 @@
-import { Component, OnInit, Input, OnChanges } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, forwardRef } from '@angular/core';
+import { NG_VALUE_ACCESSOR } from '@angular/forms';
 import EditorConfig from '../config-interface';
 @Component({
   selector: 'app-editor-container',
   templateUrl: './editor-container.component.html',
-  styleUrls: ['./editor-container.component.less']
+  styleUrls: ['./editor-container.component.less'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => EditorContainerComponent),
+      multi: true
+    }
+  ]
 })
 export class EditorContainerComponent implements OnInit, OnChanges {
   @Input() editorConfig: EditorConfig;
+
+  html: string;
   constructor() {
     this.editorConfig = {
       file: false,
@@ -19,6 +29,29 @@ export class EditorContainerComponent implements OnInit, OnChanges {
       fontColor: false,
       highlightColor: false
     };
+  }
+
+  onChange: any = () => {};
+  onTouch: any = () => {};
+
+  set htmlVal(html) {
+      if ( html !== null && html !== undefined && this.html !== html) {
+        this.html = html;
+        this.onChange(html);
+        this.onTouch(html);
+      }
+  }
+
+  writeValue(value: any): void {
+    this.htmlVal = value;
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouch = fn;
   }
 
   ngOnInit(): void {
