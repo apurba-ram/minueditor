ee1b4dd28ca497880b894995744eb373562cff77
Apurba <apurba@excellencetech.com>
2021-02-15 17:25:16 +0530

    Updatte

--- src/app/melo-mini-editor/editor-container/editor-container.component.ts
+++ src/app/melo-mini-editor/editor-container/editor-container.component.ts
@@ -3,199 +3,199 @@ import { NG_VALUE_ACCESSOR } from '@angular/forms';
 import EditorConfig from '../config-interface';
 import { nanoid } from 'nanoid';
 @Component({
-  selector: 'app-editor-container',
-  templateUrl: './editor-container.component.html',
-  styleUrls: ['./editor-container.component.less'],
-  providers: [
-    {
-      provide: NG_VALUE_ACCESSOR,
-      useExisting: forwardRef(() => EditorContainerComponent),
-      multi: true
-    }
-  ],
-  changeDetection: ChangeDetectionStrategy.OnPush
+    selector: 'app-editor-container',
+    templateUrl: './editor-container.component.html',
+    styleUrls: ['./editor-container.component.less'],
+    providers: [
+        {
+            provide: NG_VALUE_ACCESSOR,
+            useExisting: forwardRef(() => EditorContainerComponent),
+            multi: true
+        }
+    ],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class EditorContainerComponent implements OnInit, OnChanges {
-  @Input() editorConfig: EditorConfig;
+    @Input() editorConfig: EditorConfig;
 
-  html: string;
-  innerText: string;
-  lastChar: string;
-  sel: any;
-  startOffset: number;
-  endOffset: number;
-  id: string;
-  format: boolean;
-  node: any;
-  tribute: string;
-  flag: number;
-  placeholder: string;
-  mentionConfig: any;
-  mentionid: number | string;
-  mentionedNames: {id: number, name: string}[];
-  mentionedDates: string[];
-  toolbarPlacement: 'top' | 'bottom';
+    html: string;
+    innerText: string;
+    lastChar: string;
+    sel: any;
+    startOffset: number;
+    endOffset: number;
+    id: string;
+    format: boolean;
+    node: any;
+    tribute: string;
+    flag: number;
+    placeholder: string;
+    mentionConfig: any;
+    mentionid: number | string;
+    mentionedNames: { id: number, name: string }[];
+    mentionedDates: string[];
+    toolbarPlacement: 'top' | 'bottom';
 
-  constructor() {
-    this.editorConfig = {
-      file: false,
-      mentionedNames: [],
-      mentionedDates: [],
-      colorPalette: ['#FF5630', '#000000', '#414141', '#36B37E',
-                     '#6554C0', '#FF7A00', '#008299', ' #1E5DD3',
-                     '#F0B819', '#00FFF7'],
-      buttonName: '',
-      fontColor: false,
-      highlightColor: false,
-      placeholder: '',
-      toolbarPlacement: 'top'
-    };
-    this.toolbarPlacement = 'bottom';
-    this.placeholder = '';
-    this.id = nanoid();
-  }
-
-  onChange: any = () => {};
-  onTouch: any = () => {};
-
-  set htmlVal(html) {
-      if ( html !== null && html !== undefined && this.html !== html) {
-        this.html = html;
-        this.onChange(html);
-        this.onTouch(html);
-      }
-  }
-
-  writeValue(value: any): void {
-    this.htmlVal = value;
-  }
-
-  registerOnChange(fn: any): void {
-    this.onChange = fn;
-  }
-
-  registerOnTouched(fn: any): void {
-    this.onTouch = fn;
-  }
-
-  ngOnInit(): void {
-  }
-
-  ngOnChanges(changes: SimpleChanges): void {
-    if (changes.editorConfig && this.editorConfig) {
-      this.placeholder = this.editorConfig?.placeholder ?? '';
-
-      this.mentionConfig = {};
-      if (Array.isArray(this.editorConfig?.mentionedNames) && this.editorConfig?.mentionedNames.length > 0) {
-        this.editorConfig.mentionedNames = this.editorConfig?.mentionedNames.filter((item: {id: number, name: string} )=> {
-          if (item.id !== 0 && item.name.trim() !== '') {
-            return item;
-          }
-        });
-
-        this.mentionConfig.mentions = [];
-        this.mentionConfig.mentions.push({
-          items: this.editorConfig.mentionedNames,
-          triggerChar: '@',
-          mentionSelect: (item) => {
-            this.tribute = `@${item.name}`;
-            this.mentionid = item.id;
-            return this.tribute;
-          },
-          labelKey: 'name',
-          maxItems: 20,
-          disableSearch: false,
-          dropUp: true
-        });
-      }
-      if (Array.isArray(this.editorConfig?.mentionedDates) && this.editorConfig?.mentionedDates.length > 0) {
-        this.editorConfig.mentionedDates = [...new Set(this.editorConfig?.mentionedDates)];
-        this.mentionConfig.mentions.push({
-          items: this.editorConfig.mentionedDates,
-          triggerChar: '#',
-          mentionSelect: (item) => {
-            this.tribute = `#${item.date}`;
-            this.mentionid = item.date;
-            return this.tribute;
-          },
-          labelKey: 'date',
-          maxItems: 20,
-          disableSearch: false,
-          dropUp: true
-        });
-      }
-    }
-  }
-
-  hello(): void {
-    console.log('HELLO2');
-  }
-
-  getPrecedingCharacter(container: any): string { // get last input character
-    const r = this.sel.getRangeAt(0).cloneRange();
-    r.setStart(container, 0);
-    return r.toString().slice(-1);
-  }
-
-  blur(): void {
-  }
-
-  setValue(innerText: string, innerHtml: string): void {
-
-    this.innerText = innerText;
-
-    if (this.innerText === '') {
-      document.execCommand('removeFormat', false, ''); // remove previous format once the editor is clear
-    }
-    this.lastChar = this.getPrecedingCharacter(window.getSelection().anchorNode); // gets the last input character
-
-    if (this.format && this.startOffset && this.tribute) {
-      this.format = false;
-      this.endOffset = this.sel.getRangeAt(0).endOffset;
-
-      const range = document.createRange();
-      range.setStart(this.node, this.startOffset - 1);
-      range.setEnd(this.node, this.endOffset);
-      range.deleteContents(); // deleting previous set contents
+    constructor() {
+        this.editorConfig = {
+            file: false,
+            mentionedNames: [],
+            mentionedDates: [],
+            colorPalette: ['#FF5630', '#000000', '#414141', '#36B37E',
+                '#6554C0', '#FF7A00', '#008299', ' #1E5DD3',
+                '#F0B819', '#00FFF7'],
+            buttonName: '',
+            fontColor: false,
+            highlightColor: false,
+            placeholder: '',
+            toolbarPlacement: 'top'
+        };
+        this.toolbarPlacement = 'bottom';
+        this.placeholder = '';
+        this.id = nanoid();
     }
 
-    if (this.lastChar === '@' || this.lastChar === '#') {
-      this.node = this.sel.anchorNode;
-      this.format = true;
-      this.flag = this.lastChar === '@' ? 0 : 1;
-      this.startOffset = this.sel.getRangeAt(0).startOffset;
+    onChange: any = () => { };
+    onTouch: any = () => { };
+
+    set htmlVal(html) {
+        if (html !== null && html !== undefined && this.html !== html) {
+            this.html = html;
+            this.onChange(html);
+            this.onTouch(html);
+        }
     }
 
-    this.writeValue(document.getElementById(`${this.id}`).innerHTML);
-  }
-
-  mentionClosed(): void { // insert mentions
-
-    if (this.tribute !== '') {
-      const input = document.createElement('input');
-      input.setAttribute('value', `${this.tribute}`);
-      input.setAttribute('type', 'button');
-      input.setAttribute('disabled', 'true');
-      input.setAttribute('data-id', `${this.mentionid}`);
-      input.setAttribute('mention-data', `${this.flag === 0 ? '@' : '#'}`);
-      input.style.border = 'none';
-      input.style.borderRadius = '2px';
-      input.style.padding = '3px';
-      input.style.backgroundColor = '#e7eff6';
-      input.style.color = '#4681ef';
-      input.style.fontWeight = 'inherit';
-      input.style.fontSize = 'inherit';
-      const range = this.sel.getRangeAt(0).cloneRange();
-      this.sel.removeAllRanges();
-      const sp = document.createTextNode(' ');
-      range.insertNode(input);
-      range.insertNode(sp);
-      range.setStartAfter(input);
-      this.sel.addRange(range);
-      this.tribute = '';
+    writeValue(value: any): void {
+        this.htmlVal = value;
     }
-   //  this.valueInput = true;
-  }
-  onPaste(e){}
+
+    registerOnChange(fn: any): void {
+        this.onChange = fn;
+    }
+
+    registerOnTouched(fn: any): void {
+        this.onTouch = fn;
+    }
+
+    ngOnInit(): void {
+    }
+
+    ngOnChanges(changes: SimpleChanges): void {
+        if (changes.editorConfig && this.editorConfig) {
+            this.placeholder = this.editorConfig?.placeholder ?? '';
+
+            this.mentionConfig = {};
+            if (Array.isArray(this.editorConfig?.mentionedNames) && this.editorConfig?.mentionedNames.length > 0) {
+                this.editorConfig.mentionedNames = this.editorConfig?.mentionedNames.filter((item: { id: number, name: string }) => {
+                    if (item.id !== 0 && item.name.trim() !== '') {
+                        return item;
+                    }
+                });
+
+                this.mentionConfig.mentions = [];
+                this.mentionConfig.mentions.push({
+                    items: this.editorConfig.mentionedNames,
+                    triggerChar: '@',
+                    mentionSelect: (item) => {
+                        this.tribute = `@${item.name}`;
+                        this.mentionid = item.id;
+                        return this.tribute;
+                    },
+                    labelKey: 'name',
+                    maxItems: 20,
+                    disableSearch: false,
+                    dropUp: true
+                });
+            }
+            if (Array.isArray(this.editorConfig?.mentionedDates) && this.editorConfig?.mentionedDates.length > 0) {
+                this.editorConfig.mentionedDates = [...new Set(this.editorConfig?.mentionedDates)];
+                this.mentionConfig.mentions.push({
+                    items: this.editorConfig.mentionedDates,
+                    triggerChar: '#',
+                    mentionSelect: (item) => {
+                        this.tribute = `#${item.date}`;
+                        this.mentionid = item.date;
+                        return this.tribute;
+                    },
+                    labelKey: 'date',
+                    maxItems: 20,
+                    disableSearch: false,
+                    dropUp: true
+                });
+            }
+        }
+    }
+
+    hello(): void {
+        console.log('HELLO2');
+    }
+
+    getPrecedingCharacter(container: any): string { // get last input character
+        const r = this.sel.getRangeAt(0).cloneRange();
+        r.setStart(container, 0);
+        return r.toString().slice(-1);
+    }
+
+    blur(): void {
+    }
+
+    setValue(innerText: string, innerHtml: string): void {
+
+        this.innerText = innerText;
+
+        if (this.innerText === '') {
+            document.execCommand('removeFormat', false, ''); // remove previous format once the editor is clear
+        }
+        this.lastChar = this.getPrecedingCharacter(window.getSelection().anchorNode); // gets the last input character
+
+        if (this.format && this.startOffset && this.tribute) {
+            this.format = false;
+            this.endOffset = this.sel.getRangeAt(0).endOffset;
+
+            const range = document.createRange();
+            range.setStart(this.node, this.startOffset - 1);
+            range.setEnd(this.node, this.endOffset);
+            range.deleteContents(); // deleting previous set contents
+        }
+
+        if (this.lastChar === '@' || this.lastChar === '#') {
+            this.node = this.sel.anchorNode;
+            this.format = true;
+            this.flag = this.lastChar === '@' ? 0 : 1;
+            this.startOffset = this.sel.getRangeAt(0).startOffset;
+        }
+
+        this.writeValue(document.getElementById(`${this.id}`).innerHTML);
+    }
+
+    mentionClosed(): void { // insert mentions
+
+        if (this.tribute !== '') {
+            const input = document.createElement('input');
+            input.setAttribute('value', `${this.tribute}`);
+            input.setAttribute('type', 'button');
+            input.setAttribute('disabled', 'true');
+            input.setAttribute('data-id', `${this.mentionid}`);
+            input.setAttribute('mention-data', `${this.flag === 0 ? '@' : '#'}`);
+            input.style.border = 'none';
+            input.style.borderRadius = '2px';
+            input.style.padding = '3px';
+            input.style.backgroundColor = '#e7eff6';
+            input.style.color = '#4681ef';
+            input.style.fontWeight = 'inherit';
+            input.style.fontSize = 'inherit';
+            const range = this.sel.getRangeAt(0).cloneRange();
+            this.sel.removeAllRanges();
+            const sp = document.createTextNode(' ');
+            range.insertNode(input);
+            range.insertNode(sp);
+            range.setStartAfter(input);
+            this.sel.addRange(range);
+            this.tribute = '';
+        }
+        //  this.valueInput = true;
+    }
+    onPaste(e) { }
 
 }
